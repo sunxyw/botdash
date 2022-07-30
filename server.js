37f4d651b650512914ef8517e586e92b15d0e544
sunxyw <31698606+sunxyw@users.noreply.github.com>
2022-07-30 13:58:06 +0000

    setup template

--- server.js
+++ server.js
@@ -3,18 +3,18 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 // Constants
 const PORT = 3000;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
+app.get("/", (req, res) => {
+  res.send("Hello remote world!\n");
 });
 
 app.listen(PORT, HOST);
